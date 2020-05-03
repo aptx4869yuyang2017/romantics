814a2180b324cbef5bad86990cfc342832295b77
yuyang <aptx4869yuyang2017@gmail.com>
2020-05-03 16:37:31 +0800

    Factor Scalar 调整

--- TinyScript/src/parser/ast/Factor.js
+++ TinyScript/src/parser/ast/Factor.js
@@ -1,23 +1,26 @@
-const ASTNode = require("./ASTNode");
-const ASTNodeType = require("./ASTNodeType");
-const TokenType = require("../../lexer/TokenType");
+const ASTNode = require('./ASTNode');
+const ASTNodeTypes = require('./ASTNodeTypes');
 
 class Factor extends ASTNode {
-  constructor(parent, it) {
-    super(parent, it);
-    const token = it.next();
-
-    var type = token.getType();
-
-    if (type === TokenType.VARIABLE) {
-      this.type = ASTNodeType.VARIABLE;
-    } else {
-      this.type = ASTNodeType.SCALAR;
-    }
-
+  constructor(token) {
+    super();
     this.label = token.getValue();
     this.lexeme = token;
   }
 }
 
 module.exports = Factor;
+const { Variable, Scalar } = require('./index');
+
+Factor.parse = it => {
+  const token = it.peek();
+  const type = token.getType();
+
+  if (type == ASTNodeTypes.VARIABLE) {
+    it.next();
+    return new Variable(token);
+  } else if (token.isScalar()) {
+    it.next();
+    return new Scalar(token);
+  }
+};
