f87051250da87b8dbf35bed9f9dfa3f851a66359
yuyang <aptx4869yuyang2017@gmail.com>
2020-05-03 20:38:11 +0800

    assignStmt

--- TinyScript/src/parser/ast/AssignStmt.js
+++ TinyScript/src/parser/ast/AssignStmt.js
@@ -1,10 +1,31 @@
-const Stmt = require("./Stmt.js");
-const ASTNodeType = require("./ASTNodeType");
+const Stmt = require('./Stmt.js');
+const ASTNodeTypes = require('./ASTNodeTypes');
+const ParseException = require('../util/ParseException');
 
 class AssignStmt extends Stmt {
-  constructor(parent) {
-    super(parent, ASTNodeType.ASSIGN_STMT, "assign");
+  constructor() {
+    super(ASTNodeTypes.ASSIGN_STMT, 'assign');
   }
 }
 
 module.exports = AssignStmt;
+
+const { Factor, Expr } = require('./index');
+
+AssignStmt.parse = it => {
+  const stmt = new AssignStmt();
+  const token = it.peek();
+  const factor = Factor.parse(it);
+
+  if (factor == null) {
+    throw ParseException.fromToken(token);
+  }
+
+  stmt.addChild(factor);
+  const lexeme = it.nextMatch('=');
+  const expr = Expr.parse(it);
+  stmt.addChild(expr);
+  stmt.setLexeme(lexeme);
+
+  return stmt;
+};
